68d8eebd9f0ae5f7f763f32a82f5141d594ca57f
roobeen-b <rubinbaidhya@gmail.com>
2024-09-25 12:20:18 +0545

    Added sentry for more robust error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a32e2e9aa7341ce2a8ff566d69444434@o4507836535537664.ingest.us.sentry.io/4508012070961152",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
