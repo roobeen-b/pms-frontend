845e853de4a1433d3b4487613a174280b083ec73
roobeen-b <rubinbaidhya@gmail.com>
2024-11-10 13:51:39 +0545

    Added signout popup modal

---
+++ components/SignoutModal.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const SignoutModal = () => {
+  const router = useRouter();
+  const [open, setOpen] = useState(true);
+
+  function handleSignOut() {
+    localStorage.clear();
+    router.push("/");
+  }
+
+  return (
+    <AlertDialog open={open}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={() => setOpen(false)}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleSignOut}>
+            Sign Out
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default SignoutModal;
